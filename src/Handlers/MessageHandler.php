172172f24721233758293194497a7a32d28111bc
Jonas Stendahl <jonas.stendahl@gmail.com>
2015-09-28 16:51:28 +0200

    Silly merge

--- src/Handlers/MessageHandler.php
+++ src/Handlers/MessageHandler.php
@@ -14,38 +14,10 @@ class MessageHandler extends HandlerAbstract
             return;
         }
 
-<<<<<<< 04c925962472ec337e74183da16ea2aade0a3130
-        if (isset($payload->subtype) === true) {
-            if (isset($this->types[$payload->subtype]) === true) {
-                foreach ($this->types[$payload->subtype] as $handler) {
-                    call_user_func($handler, $payload);
-                }
-            }
-        }
-
-        if (isset($payload->text) === true) {
-            foreach ($this->triggers as $trigger => $handlers) {
-                if (preg_match_all($trigger, $payload->text, $matches) > 0) {
-                    foreach ($handlers as $handler) {
-                        try {
-                            call_user_func($handler, $payload, $matches);
-                        } catch (\Exception $e) {
-                            $this->pingu['logger']->addError($e);
-                            if (isset($payload->channel) and isset($payload->user) and $payload->channel !== $this->pingu['config']['slack']['channel']) {
-                                $user         = $this->pingu['slack']->getUser($payload->user);
-                                $errorMessage = '@'.$user['name'].': Your message made me crash unexpectedly. Thanks for that!';
-   
-                                $this->pingu['slack']->sendMessage($payload->channel, $errorMessage);
-                            }
-                        }
-                    }
-                }
-=======
         foreach ($this->pingu['types'] as $potentialHandler) {
             if ($potentialHandler->getEvent() === $payload->type) {
                 $handler = $potentialHandler;
                 continue;
->>>>>>> Use WebSockets through PHP to improve latency
             }
         }
 
